f95c6fbbd15b8a15c4b51c5645fd6ee207f0a8e9
Ikedieze Onyeforo <iykedapotential@gmail.com>
2016-09-17 12:10:35 +0100

    fixed api routing

--- models/restaurants.js
+++ models/restaurants.js
@@ -4,10 +4,13 @@
 var mongoose = require('mongoose');
 
 var restaurantSchema = new mongoose.Schema({
-    name: {type:String,required:true},
-    address: {type: String,required:true},
-    email:{type:String, required:true},
-    phone:{type:Number}
+    name: {
+        type: String,
+        required: true
+    },
+    address: {type: String, required: true},
+    email: {type: String, required: true},
+    phone: {type: String}
 });
 
 module.exports = mongoose.model('restaurant', restaurantSchema);
